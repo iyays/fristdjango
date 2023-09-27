1ee1d3d6b7970ad5a5ba549113cc2624e4e81cb1
iyays <142280437+iyays@users.noreply.github.com>
2023-09-27 15:30:22 +0700

    Add files via upload

---
+++ mysite/polls/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from polls.models import Question, Choice
+# Register your models here.
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_filter = ['pub_date']
+    search_fields = ["question_text"]
+
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
